2063ce9bb1dc63fadec09587875cf5ed87f7ff6c
alfredodcastro@hotmail.com <alfredodcastro@hotmail.com>
2023-06-16 19:03:04 -0500

    Add project files.

---
+++ Commerce.Web/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using Commerce.Web.Data;
+using Commerce.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Commerce.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        /// <summary>
+        /// Index
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            ViewBag.ProductDescription = "Libros nuevos"; 
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+    }
+}
---
+++ Commerce.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Commerce.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Commerce.Web.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            :base(options)
+        {
+
+        }
+        public DbSet<Category>Categories { get; set; }
+    }
+}
