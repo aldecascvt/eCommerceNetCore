3f0ec151c10725a643bc90be95a0842a5bcefd58
alfredodcastro@hotmail.com <alfredodcastro@hotmail.com>
2023-06-21 20:13:22 -0500

    Category

--- Commerce.Web/Controllers/CategoryController.cs
+++ Commerce.Web/Controllers/CategoryController.cs
@@ -17,9 +17,85 @@ namespace Commerce.Web.Controllers
         /// <returns></returns>
         public IActionResult Index()
         {
-            ViewBag.ProductDescription = "Libros nuevos"; 
+            ViewBag.ProductDescription = "Libros nuevos";
             IEnumerable<Category> objCategoryList = _db.Categories;
             return View(objCategoryList);
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category Created Successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var objToEdit = _db.Categories.Find(id);
+            if (objToEdit == null)
+            {
+                return NotFound();
+            }
+
+            return View(objToEdit);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category Updated Successfully";
+                return RedirectToAction("Index");
+
+            }
+            return View(obj);
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null | id == 0)
+            {
+                return NotFound();
+            }
+            var objToEdit = _db.Categories.Find(id);
+            if (objToEdit == null)
+            {
+                return NotFound();
+            }
+
+            return View(objToEdit);
+
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteObj(int? id)
+        {
+            var obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category Deleted Successfully";
+            return RedirectToAction("Index");
+        }
+
     }
 }
